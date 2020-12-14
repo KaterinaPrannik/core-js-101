4788a199032a1498b07c95855b4f0ec6dabfd537
KaterinaPrannik <60596523+KaterinaPrannik@users.noreply.github.com>
2020-12-14 21:47:09 +0300

    fix: solve some task

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -28,10 +28,16 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  const answer = new Promise((resolve, reject) => {
+    if (isPositiveAnswer === true) {
+      resolve('Hooray!!! She said "Yes"!');
+    } else if (isPositiveAnswer === false) {
+      resolve('Oh no, she said "No".');
+    } else reject(new Error('Wrong parameter is passed! Ask her again.'));
+  });
+  return answer;
 }
-
 
 /**
  * Return Promise object that should be resolved with array containing plain values.
